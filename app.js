d2e292e6736d1d557b407548b6dbe981dba250a0
nathangreiling <ngreiling20@gmail.com>
2022-03-31 17:38:04 -0500

    file structure

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
 
-// Notice the lack of parentheses around the `profileDataArr` parameter?
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i +=1) {
-      console.log(profileDataArr[i]);
-    }
-  console.log('========');
-  printProfileData(profileDataArgs)
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-      
-};
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
